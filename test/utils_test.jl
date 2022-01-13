4986b869bb6ab59ceb1a46b81cc9b47d937afcee
xtalax <alex.lemony@gmail.com>
2022-01-13 17:55:27 +0000

    readded util tests

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -7,24 +7,24 @@ using MethodOfLines, Test, ModelingToolkit
 
     Dx = Differential(x)
     eq  = Dt(u(t,x)) ~ -Dx(u(t,x))
-    @test first(DiffEqOperators.differential_order(eq.rhs, x.val)) == 1
-    @test isempty(DiffEqOperators.differential_order(eq.rhs, t.val))
-    @test first(DiffEqOperators.differential_order(eq.lhs, t.val)) == 1
-    @test isempty(DiffEqOperators.differential_order(eq.lhs, x.val))
+    @test first(MethodOfLines.differential_order(eq.rhs, x.val)) == 1
+    @test isempty(MethodOfLines.differential_order(eq.rhs, t.val))
+    @test first(MethodOfLines.differential_order(eq.lhs, t.val)) == 1
+    @test isempty(MethodOfLines.differential_order(eq.lhs, x.val))
 
     Dxx = Differential(x)^2
     eq  = Dt(u(t,x)) ~ Dxx(u(t,x))
-    @test first(DiffEqOperators.differential_order(eq.rhs, x.val)) == 2
-    @test isempty(DiffEqOperators.differential_order(eq.rhs, t.val))
-    @test first(DiffEqOperators.differential_order(eq.lhs, t.val)) == 1
-    @test isempty(DiffEqOperators.differential_order(eq.lhs, x.val))
+    @test first(MethodOfLines.differential_order(eq.rhs, x.val)) == 2
+    @test isempty(MethodOfLines.differential_order(eq.rhs, t.val))
+    @test first(MethodOfLines.differential_order(eq.lhs, t.val)) == 1
+    @test isempty(MethodOfLines.differential_order(eq.lhs, x.val))
 
     Dxxxx = Differential(x)^4
     eq  = Dt(u(t,x)) ~ -Dxxxx(u(t,x))
-    @test first(DiffEqOperators.differential_order(eq.rhs, x.val)) == 4
-    @test isempty(DiffEqOperators.differential_order(eq.rhs, t.val))
-    @test first(DiffEqOperators.differential_order(eq.lhs, t.val)) == 1
-    @test isempty(DiffEqOperators.differential_order(eq.lhs, x.val))
+    @test first(MethodOfLines.differential_order(eq.rhs, x.val)) == 4
+    @test isempty(MethodOfLines.differential_order(eq.rhs, t.val))
+    @test first(MethodOfLines.differential_order(eq.lhs, t.val)) == 1
+    @test isempty(MethodOfLines.differential_order(eq.lhs, x.val))
 end
 
 @testset "count differentials 2D" begin
@@ -35,12 +35,12 @@ end
     Dt = Differential(t)
 
     eq  = Dt(u(t,x,y)) ~ Dxx(u(t,x,y)) + Dyy(u(t,x,y))
-    @test first(DiffEqOperators.differential_order(eq.rhs, x.val)) == 2
-    @test first(DiffEqOperators.differential_order(eq.rhs, y.val)) == 2
-    @test isempty(DiffEqOperators.differential_order(eq.rhs, t.val))
-    @test first(DiffEqOperators.differential_order(eq.lhs, t.val)) == 1
-    @test isempty(DiffEqOperators.differential_order(eq.lhs, x.val))
-    @test isempty(DiffEqOperators.differential_order(eq.lhs, y.val))
+    @test first(MethodOfLines.differential_order(eq.rhs, x.val)) == 2
+    @test first(MethodOfLines.differential_order(eq.rhs, y.val)) == 2
+    @test isempty(MethodOfLines.differential_order(eq.rhs, t.val))
+    @test first(MethodOfLines.differential_order(eq.lhs, t.val)) == 1
+    @test isempty(MethodOfLines.differential_order(eq.lhs, x.val))
+    @test isempty(MethodOfLines.differential_order(eq.lhs, y.val))
 end
 
 @testset "count with mixed terms" begin
@@ -53,8 +53,8 @@ end
     Dt = Differential(t)
 
     eq  = Dt(u(t,x,y)) ~ Dxx(u(t,x,y)) + Dyy(u(t,x,y)) + Dx(Dy(u(t,x,y)))
-    @test DiffEqOperators.differential_order(eq.rhs, x.val) == Set([2, 1])
-    @test DiffEqOperators.differential_order(eq.rhs, y.val) == Set([2, 1])
+    @test MethodOfLines.differential_order(eq.rhs, x.val) == Set([2, 1])
+    @test MethodOfLines.differential_order(eq.rhs, y.val) == Set([2, 1])
 end
 
 @testset "Kuramoto–Sivashinsky equation" begin
@@ -70,5 +70,5 @@ end
     β = 4
     γ = 1
     eq = Dt(u(x,t)) + u(x,t)*Dx(u(x,t)) + α*Dx2(u(x,t)) + β*Dx3(u(x,t)) + γ*Dx4(u(x,t)) ~ 0
-    @test DiffEqOperators.differential_order(eq.lhs, x.val) == Set([4, 3, 2, 1])
+    @test MethodOfLines.differential_order(eq.lhs, x.val) == Set([4, 3, 2, 1])
 end
