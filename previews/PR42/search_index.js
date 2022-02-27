dacf3e226050a0f0ef169329f1f2882dbc0aaaa2
Documenter.jl <documenter@juliadocs.github.io>
2022-02-27 23:19:30 +0000

    build based on 583e658

---
+++ previews/PR42/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MethodOfLines.jl","page":"MethodOfLines.jl: Automated Finite Difference for Phyiscs-Informed Learning","title":"MethodOfLines.jl","text":"","category":"section"},{"location":"","page":"MethodOfLines.jl: Automated Finite Difference for Phyiscs-Informed Learning","title":"MethodOfLines.jl: Automated Finite Difference for Phyiscs-Informed Learning","text":"MethodOfLines.jl is a package for  automated finite difference discretization of symbolically-defined PDEs","category":"page"}]
+}
