0031b6101cf213d4c0b81976f570c719de760dca
xtalax <alex.lemony@gmail.com>
2022-03-14 17:27:27 +0000

    fix bruss test

--- test/pde_systems/brusselator_eq.jl
+++ test/pde_systems/brusselator_eq.jl
@@ -57,53 +57,53 @@ using DomainSets
 
        @time sol = solve(prob, TRBDF2(),saveat=0.01)
 
-       # Nx = floor(Int64, (x_max - x_min) / dx) + 1
-       # Ny = floor(Int64, (y_max - y_min) / dy) + 1
+       Nx = floor(Int64, (x_max - x_min) / dx) + 1
+       Ny = floor(Int64, (y_max - y_min) / dy) + 1
 
-       # @variables u[1:Nx,1:Ny](t)
-       # @variables v[1:Nx,1:Ny](t)
+       @variables u[1:Nx,1:Ny](t)
+       @variables v[1:Nx,1:Ny](t)
        
-       # # Solve reference problem
+       # Solve reference problem
        
-       # N = 31
-       # xyd_brusselator = range(0,stop=1,length=N)
-       # brusselator_f(x, y, t) = (((x-0.3)^2 + (y-0.6)^2) <= 0.1^2) * (t >= 1.1) * 5.
-       # limit(a, N) = a == N+1 ? 1 : a == 0 ? N : a
-       # function brusselator_2d_loop(du, u, p, t)
-       #        A, B, alpha, dx = p
-       #        alpha = alpha/dx^2
-       #        @inbounds for I in CartesianIndices((N, N))
-       #               i, j = Tuple(I)
-       #     x, y = xyd_brusselator[I[1]], xyd_brusselator[I[2]]
-       #     ip1, im1, jp1, jm1 = limit(i+1, N), limit(i-1, N), limit(j+1, N), limit(j-1, N)
-       #     du[i,j,1] = alpha*(u[im1,j,1] + u[ip1,j,1] + u[i,jp1,1] + u[i,jm1,1] - 4u[i,j,1]) +
-       #     B + u[i,j,1]^2*u[i,j,2] - (A + 1)*u[i,j,1] + brusselator_f(x, y, t)
-       #     du[i,j,2] = alpha*(u[im1,j,2] + u[ip1,j,2] + u[i,jp1,2] + u[i,jm1,2] - 4u[i,j,2]) +
-       #                 A*u[i,j,1] - u[i,j,1]^2*u[i,j,2]
-       #     end
-       # end
-       # p = (3.4, 1., 10., step(xyd_brusselator))
+       N = 32
+       xyd_brusselator = range(0,stop=1,length=N)
+       brusselator_f(x, y, t) = (((x-0.3)^2 + (y-0.6)^2) <= 0.1^2) * (t >= 1.1) * 5.
+       limit(a, N) = a == N+1 ? 1 : a == 0 ? N : a
+       function brusselator_2d_loop(du, u, p, t)
+              A, B, alpha, dx = p
+              alpha = alpha/dx^2
+              @inbounds for I in CartesianIndices((N, N))
+                     i, j = Tuple(I)
+           x, y = xyd_brusselator[I[1]], xyd_brusselator[I[2]]
+           ip1, im1, jp1, jm1 = limit(i+1, N), limit(i-1, N), limit(j+1, N), limit(j-1, N)
+           du[i,j,1] = alpha*(u[im1,j,1] + u[ip1,j,1] + u[i,jp1,1] + u[i,jm1,1] - 4u[i,j,1]) +
+           B + u[i,j,1]^2*u[i,j,2] - (A + 1)*u[i,j,1] + brusselator_f(x, y, t)
+           du[i,j,2] = alpha*(u[im1,j,2] + u[ip1,j,2] + u[i,jp1,2] + u[i,jm1,2] - 4u[i,j,2]) +
+                       A*u[i,j,1] - u[i,j,1]^2*u[i,j,2]
+           end
+       end
+       p = (3.4, 1., 10., step(xyd_brusselator))
        
-       # function init_brusselator_2d(xyd)
-       #     N = length(xyd)
-       #     u = zeros(N, N, 2)
-       #     for I in CartesianIndices((N, N))
-       #         x = xyd[I[1]]
-       #         y = xyd[I[2]]
-       #         u[I,1] = 22*(y*(1-y))^(3/2)
-       #         u[I,2] = 27*(x*(1-x))^(3/2)
-       #     end
-       #     u
-       # end
-       # u0_manual = init_brusselator_2d(xyd_brusselator)
-       # prob = ODEProblem(brusselator_2d_loop,u0_manual,(0.,11.5),p)
+       function init_brusselator_2d(xyd)
+           N = length(xyd)
+           u = zeros(N, N, 2)
+           for I in CartesianIndices((N, N))
+               x = xyd[I[1]]
+               y = xyd[I[2]]
+               u[I,1] = 22*(y*(1-y))^(3/2)
+               u[I,2] = 27*(x*(1-x))^(3/2)
+           end
+           u
+       end
+       u0_manual = init_brusselator_2d(xyd_brusselator)
+       prob = ODEProblem(brusselator_2d_loop,u0_manual,(0.,11.5),p)
        
-       # msol = solve(prob,TRBDF2(),saveat=0.01) # 2.771 s (5452 allocations: 65.73 MiB)
+       msol = solve(prob,TRBDF2(),saveat=0.01) # 2.771 s (5452 allocations: 65.73 MiB)
        
-       # t = sol[t]
-       # for k in 1:length(t)
-       #        @test msol.u[k][:,:,1] ≈ reshape([sol[u[(i-1)*Ny+j]][k] for i in 1:Nx for j in 1:Ny],(Nx,Ny))[2:end,2:end] rtol = 0.1
-       #        @test msol.u[k][:,:,2] ≈ reshape([sol[v[(i-1)*Ny+j]][k] for i in 1:Nx for j in 1:Ny],(Nx,Ny))[2:end,2:end] rtol = 0.1
+       t = sol[t]
+       for k in 1:length(t)
+              @test msol.u[k][:,:,1] ≈ reshape([sol[u[(i-1)*Ny+j]][k] for i in 1:Nx for j in 1:Ny],(Nx,Ny))[2:end,2:end] rtol = 0.1
+              @test msol.u[k][:,:,2] ≈ reshape([sol[v[(i-1)*Ny+j]][k] for i in 1:Nx for j in 1:Ny],(Nx,Ny))[2:end,2:end] rtol = 0.1
        end
    
        
