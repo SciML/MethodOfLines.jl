152448a390d227bbeb10e2d091650259499cb5bd
xtalax <alex.lemony@gmail.com>
2022-04-27 14:12:15 +0100

    assert that t is a Num or Nothing

--- src/MOLFiniteDifference.jl
+++ src/MOLFiniteDifference.jl
@@ -8,12 +8,14 @@ end
 
 # Constructors. If no order is specified, both upwind and centered differences will be 2nd order
 function MOLFiniteDifference(dxs, time=nothing; approx_order = 2, upwind_order = 1, grid_align=CenterAlignedGrid())
-    
+
     if approx_order % 2 != 0
         @warn "Discretization approx_order must be even, rounding up to $(approx_order+1)"
     end
     @assert approx_order >= 1 "approx_order must be at least 1"
     @assert upwind_order >= 1 "upwind_order must be at least 1"
-    
+
+    @assert (t isa Num | t isa Nothing) "time must be a Num, or Nothing."
+
     return MOLFiniteDifference{typeof(grid_align)}(dxs, time, approx_order, upwind_order, grid_align)
 end
