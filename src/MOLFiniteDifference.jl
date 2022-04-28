bcdd7e8d06943f3523458fcc2567abeb2311fae6
xtalax <alex.lemony@gmail.com>
2022-04-28 21:35:08 +0100

    start overlap

--- src/MOLFiniteDifference.jl
+++ src/MOLFiniteDifference.jl
@@ -1,13 +1,14 @@
 struct MOLFiniteDifference{G} <: DiffEqBase.AbstractDiscretization
     dxs
     time
+    overlap_map
     approx_order::Int
     upwind_order::Int
     grid_align::G
 end
 
 # Constructors. If no order is specified, both upwind and centered differences will be 2nd order
-function MOLFiniteDifference(dxs, time=nothing; approx_order = 2, upwind_order = 1, grid_align=CenterAlignedGrid())
+function MOLFiniteDifference(dxs, time=nothing; approx_order = 2, upwind_order = 1, grid_align=CenterAlignedGrid(), overlap_map = [])
 
     if approx_order % 2 != 0
         @warn "Discretization approx_order must be even, rounding up to $(approx_order+1)"
@@ -17,5 +18,11 @@ function MOLFiniteDifference(dxs, time=nothing; approx_order = 2, upwind_order =
 
     @assert (time isa Num) | (time isa Nothing) "time must be a Num, or Nothing - got $(typeof(time)). See docs for MOLFiniteDifference."
 
-    return MOLFiniteDifference{typeof(grid_align)}(dxs, time, approx_order, upwind_order, grid_align)
+    overlap_map = Dict(mapreduce(vcat, keys(overlap_map)) do x
+        [y => x for y in overlap_map[x]]
+    end)
+
+    @assert collect(keys(overlap_map)) == unique(collect(keys(overlap_map))) "Variables cannot be subsets of more than one domain, check overlap_map."
+
+    return MOLFiniteDifference{typeof(grid_align)}(dxs, time, overlap_map, approx_order, upwind_order, grid_align)
 end
