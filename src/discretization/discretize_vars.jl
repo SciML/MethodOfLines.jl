1450019ad48bb74ae2fcc58c813197c49ccb2078
xtalax <alex.lemony@gmail.com>
2022-05-09 19:28:11 +0100

    support overlapping domains

--- src/discretization/discretize_vars.jl
+++ src/discretization/discretize_vars.jl
@@ -14,14 +14,9 @@ end
 
 # * The move to DiscretizedVariable with a smart recursive getindex and custom dict based index type (?) will allow for sampling whole expressions at once, leading to much greater flexibility. Both Sym and Array interfaces will be implemented. Derivatives become the demarcation between different types of sampling => Derivatives are a custom subtype of DiscretizedVariable, with special subtypes for Nonlinear laplacian/spherical/ other types of derivatives with special handling. There is a pre discretized equation step that recognizes and replaces these with rules, and then the resulting equation is simply indexed into to generate the interior/BCs.
 
-function DiscreteSpace(domain, depvars, x̄, discretization::MOLFiniteDifference{G}) where {G}
-    t = discretization.time
-    nspace = length(x̄)
-    # Discretize space
+# TODO: Allow other grids
 
-    # Needed to allow variables in seperate but overlapping domains
-    subdomains = filter(y -> any(isequal((y,), unique(reduce(vcat, [discretization.overlap_map[x] for x in keys(discretization.overlap_map)])))), x̄)
-    superdomains = filter(y -> !any(isequal.(subdomains,(y,))), x̄)
+@inline function generate_axies(superdomains, domain, discretization)
     subaxies = []
     axies = map(superdomains) do x
         xdomain = domain[findfirst(d -> isequal(x, d.variables), domain)]
@@ -45,53 +40,97 @@ function DiscreteSpace(domain, depvars, x̄, discretization::MOLFiniteDifference
                 end)
                 for high in values(highs)
                     if !(high ∈ discx)
-                        discx = vcat(discx[discx .< high], high, discx[discx .> high])
+                        discx = vcat(discx[discx.<high], high, discx[discx.>high])
                         @warn "Due to overlap, adding grid point at $x = $(high))."
                     end
                 end
                 for low in values(lows)
                     if !(low ∈ discx)
-                        discx = vcat(discx[discx .< low], low, discx[discx .> low])
+                        discx = vcat(discx[discx.<low], low, discx[discx.>low])
                         @warn "Due to overlap, adding grid point at $x = $(low))."
                     end
                 end
             end
-            vcat!(subaxies, map(y -> y => discx[lows[y] .<= discx .<= highs[y]], xsub))
+            vcat!(subaxies, map(y -> y.val => discx[lows[y].<=discx.<=highs[y]], xsub))
         catch e
             # Catch missing keys for no overlap
         end
 
         x => discx
     end
+    return Dict(vcat(subaxies, axies))
+end
 
-    axies = Dict(vcat(subaxies, axies))
-    #* intercept overlapping variables and define them as views into parent grid
-    #* check that endpoints of the domains are represented
-
-    # Define the grid on which the dependent variables will be evaluated (see #378)
-    # center_align is recommended for Dirichlet BCs
-    # edge_align is recommended for Neumann BCs (spatial discretization is conservative)
-
-    grid = generate_grid(x̄, axies, domain, discretization)
-
-    dxs = map(x̄) do x
-        discx = Dict(grid)[x]
-        if discx isa StepRangeLen
-            x => discretization.dxs[findfirst(dxs -> isequal(x, dxs[1].val), discretization.dxs)][2]
-        elseif discx isa AbstractVector # is an abstract vector but not StepRangeLen
-            x => [discx[i+1] - discx[i] for i in 1:length(x)]
-        else
-            throw(ArgumentError("Supplied d$x is not a Number or AbstractVector, got $(typeof(discretization.dxs[findfirst(dxs -> isequal(x, dxs[1].val), discretization.dxs)][2])) for $x"))
+@inline function generate_grid(superdomains, axies, domain, discretization::MOLFiniteDifference{G}) where {G<:CenterAlignedGrid}
+    imap = []
+    for x in superdomains
+        try
+            xsub = discretization.overlap_map[x]
+            for y in xsub
+                high = DomainSets.supremum(domain[findfirst(d -> isequal(x, d.variables), domain)].domain)
+                low = DomainSets.infimum(domain[findfirst(d -> isequal(x, d.variables), domain)].domain)
+                ilow = findlast(l -> l < low, axies[x])
+                ihigh = findlast(l -> l > high, discx)
+                push!(imap, y => (ilow, ihigh))
+            end
+        catch e
         end
+        push!(imap, x => (1, length(discx)))
     end
 
-    grid = Dict(grid)
+    return axies, Dict(imap)
+end
 
-    # Build symbolic variables
-    Iaxies = [u => CartesianIndices(((axes(axies[x])[1] for x in remove(arguments(u), t))...,)) for u in depvars]
-    Igrid = [u => CartesianIndices(((axes(grid[x])[1] for x in remove(arguments(u), t))...,)) for u in depvars]
 
-    depvarsdisc = map(depvars) do u
+@inline function generate_grid(superdomains, axies, domain, discretization::MOLFiniteDifference{G}) where {G<:EdgeAlignedGrid}
+    subgrid = []
+    imap = []
+    grid = map(superdomains) do x
+        xdomain = domain[findfirst(d -> isequal(x, d.variables), domain)]
+        dx = discretization.dxs[findfirst(dxs -> isequal(x, dxs[1].val), discretization.dxs)][2]
+        if dict[x] isa StepRangeLen
+            discx = (DomainSets.infimum(xdomain.domain)-dx/2):dx:(DomainSets.supremum(xdomain.domain)+dx/2)
+        else
+            discx = [(axies[x][i] + axies[x][i+1]) / 2 for i in 1:length(axies[x])-1]
+            pushfirst!(discx, discx[1] - 2 * (discx[1] - infimum(xdomain.domain)))
+            push!(discx, discx[end] + 2 * (supremum(xdomain.domain) - discx[end]))
+        end
+        # Handle overlap
+        try
+            xsub = discretization.overlap_map[x]
+            for y in xsub
+                high = DomainSets.supremum(domain[findfirst(d -> isequal(x, d.variables), domain)].domain)
+                low = DomainSets.infimum(domain[findfirst(d -> isequal(x, d.variables), domain)].domain)
+                ilow = findlast(l -> l < low, discx)
+                ihigh = findlast(l -> l > high, discx)
+                push!(imap, y => (ilow, ihigh))
+                push!(subgrid, y.val => discx[ilow:ihigh])
+            end
+        catch e
+        end
+        push!(imap, x => (1, length(discx)))
+        x => discx
+    end
+    return (Dict(vcat(subgrid, grid)), Dict(imap))
+end
+
+@inline function generate_discrete_depvars(depvars, grid, domain, discretization)
+    sub2sup = Dict(mapreduce(vcat, keys(discretization.overlap_map)) do x
+        #? needs .val?
+        [y => x for y in overlap_map[x]]
+    end)
+    superdepvars = filter(v -> isequal(v, substitute(v, sub2sup)), depvars)
+    subdepvars = setdiff(depvars, superdepvars)
+
+    sub2sup = Dict(map(depvars) do u
+        if !isequal(u, substitute(u, sub2sup))
+            u => substitute(u, sub2sup)
+        else
+            nothing => nothing
+        end
+    end)
+
+    depvarsdisc = Dict(map(superdepvars) do u
         op = SymbolicUtils.operation(u)
         if op isa SymbolicUtils.Term{SymbolicUtils.FnType{Tuple,Real},Nothing}
             sym = Symbol(string(op))
@@ -107,7 +146,49 @@ function DiscreteSpace(domain, depvars, x̄, discretization::MOLFiniteDifference
             uaxes = collect(axes(grid[x])[1] for x in remove(arguments(u), t))
             u => collect(first(@variables $sym[uaxes...](t)))
         end
+    end)
+
+    subdepvarsdisc = Dict(map(subdepvars) do u
+        args = arguments(u)
+        I = CartesianIndices(([imap[x][1]:imap[x][2] for x in args]...))
+        u => @view(depvarsdisc[sub2sup[u]][I])
+    end)
+
+    return merge(depvarsdisc, subdepvarsdisc)
+end
+
+function DiscreteSpace(domain, depvars, x̄, discretization::MOLFiniteDifference{G}) where {G}
+    t = discretization.time
+    nspace = length(x̄)
+    # Discretize space
+
+    # Needed to allow variables in seperate but overlapping domains
+    subdomains = filter(y -> any(isequal((y,), unique(reduce(vcat, [discretization.overlap_map[x] for x in keys(discretization.overlap_map)])))), x̄)
+    superdomains = filter(y -> !any(isequal.(subdomains, (y,))), x̄)
+
+    # Define the grid on which the dependent variables will be evaluated (see #378)
+    # center_align is recommended for Dirichlet BCs
+    # edge_align is recommended for Neumann BCs (spatial discretization is conservative)
+
+    axies = generate_axies(superdomains, domain, discretization)
+    grid = generate_grid(superdomains, axies, domain, discretization)
+
+    dxs = map(x̄) do x
+        discx = grid[x]
+        if discx isa StepRangeLen
+            x => discretization.dxs[findfirst(dxs -> isequal(x, dxs[1].val), discretization.dxs)][2]
+        elseif discx isa AbstractVector # is an abstract vector but not StepRangeLen
+            x => [discx[i+1] - discx[i] for i in 1:length(x)]
+        else
+            throw(ArgumentError("Supplied d$x is not a Number or AbstractVector, got $(typeof(discretization.dxs[findfirst(dxs -> isequal(x, dxs[1].val), discretization.dxs)][2])) for $x"))
+        end
     end
+
+    # Build symbolic variables
+    Iaxies = [u => CartesianIndices(((axes(axies[x])[1] for x in remove(arguments(u), t))...,)) for u in depvars]
+    Igrid = [u => CartesianIndices(((axes(grid[x])[1] for x in remove(arguments(u), t))...,)) for u in depvars]
+
+
 
     args = [operation(u) => arguments(u) for u in depvars]
 
@@ -119,8 +200,8 @@ end
 nparams(::DiscreteSpace{N,M}) where {N,M} = N
 nvars(::DiscreteSpace{N,M}) where {N,M} = M
 
-params(u,s) = remove(s.args[operation(u)], s.time)
-Base.ndims(u,s::DiscreteSpace) = length(params(u,s))
+params(u, s) = remove(s.args[operation(u)], s.time)
+Base.ndims(u, s::DiscreteSpace) = length(params(u, s))
 
 Base.length(s::DiscreteSpace, x) = length(s.grid[x])
 Base.length(s::DiscreteSpace, j::Int) = length(s.grid[s.x̄[j]])
@@ -128,7 +209,7 @@ Base.size(s::DiscreteSpace) = Tuple(length(s.grid[z]) for z in s.x̄)
 
 @inline function Idx(II, s, u, indexmap)
     # We need to construct a new index as indices may be of different size
-    length(params(u,s)) == 0 && return CartesianIndex()
+    length(params(u, s)) == 0 && return CartesianIndex()
     is = [II[indexmap[x]] for x in params(u, s)]
 
 
@@ -140,7 +221,7 @@ end
 A function that returns what to replace independent variables with in boundary equations
 """
 @inline function axiesvals(s::DiscreteSpace{N,M,G}, u_, x_, I) where {N,M,G}
-    u = depvar(u_,s)
+    u = depvar(u_, s)
     map(params(u, s)) do x
         if isequal(x, x_)
             x => (I[x2i(s, u, x)] == 1 ? first(s.axies[x]) : last(s.axies[x]))
@@ -150,8 +231,8 @@ A function that returns what to replace independent variables with in boundary e
     end
 end
 
-gridvals(s::DiscreteSpace{N}, u) where N = ndims(u,s) == 0 ? [] : map(y-> [x => s.grid[x][y.I[x2i(s, u, x)]] for x in params(u,s)],s.Igrid[u])
-gridvals(s::DiscreteSpace{N}, u, I::CartesianIndex) where N = ndims(u,s) == 0 ? [] : [x => s.grid[x][I[x2i(s,u,x)]] for x in params(u, s)]
+gridvals(s::DiscreteSpace{N}, u) where {N} = ndims(u, s) == 0 ? [] : map(y -> [x => s.grid[x][y.I[x2i(s, u, x)]] for x in params(u, s)], s.Igrid[u])
+gridvals(s::DiscreteSpace{N}, u, I::CartesianIndex) where {N} = ndims(u, s) == 0 ? [] : [x => s.grid[x][I[x2i(s, u, x)]] for x in params(u, s)]
 
 
 varmaps(s::DiscreteSpace, depvars, II, indexmap) = [u => s.discvars[u][Idx(II, s, u, indexmap)] for u in depvars]
@@ -162,26 +243,6 @@ valmaps(s, u, depvars, indexmap) = valmaps.([s], [u], [depvars], s.Igrid[u], [in
 
 map_symbolic_to_discrete(II::CartesianIndex, s::DiscreteSpace{N,M}) where {N,M} = vcat([s.ū[k] => s.discvars[k][II] for k = 1:M], [s.x̄[j] => s.grid[j][II[j]] for j = 1:N])
 
-# TODO: Allow other grids
-
-@inline function generate_grid(x̄, axies, domain, discretization::MOLFiniteDifference{G}) where {G<:CenterAlignedGrid}
-    return axies
-end
-
-@inline function generate_grid(x̄, axies, domain, discretization::MOLFiniteDifference{G}) where {G<:EdgeAlignedGrid}
-    return map(x̄) do x
-        xdomain = domain[findfirst(d -> isequal(x, d.variables), domain)]
-        dx = discretization.dxs[findfirst(dxs -> isequal(x, dxs[1].val), discretization.dxs)][2]
-        if dict[x] isa StepRangeLen
-            x => (DomainSets.infimum(xdomain.domain)-dx/2):dx:(DomainSets.supremum(xdomain.domain)+dx/2)
-        else
-            discx = [(dict[x][i]+dict[x][i+1])/2 for i in 1:length(dict[x])-1]
-            pushfirst!(discx, discx[1] - 2*(discx[1] - infimum(xdomain.domain)))
-            push!(discx, discx[end] + 2*(supremum(xdomain.domain) - discx[end]))
-            x => discx
-        end
-    end
-end
 
 
 depvar(u, s::DiscreteSpace) = operation(u)(s.args[operation(u)]...)
