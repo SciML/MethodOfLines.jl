dc0dc79bf50f5bc71289a1afcfc36442e4c016b2
xtalax <alex.lemony@gmail.com>
2022-05-18 13:26:05 +0100

    Add tests moving the term to the the side

--- test/pde_systems/MOL_1D_Linear_Convection.jl
+++ test/pde_systems/MOL_1D_Linear_Convection.jl
@@ -1,7 +1,7 @@
 # 1D linear convection problem
 
 # Packages and inclusions
-using ModelingToolkit,MethodOfLines,DiffEqBase,LinearAlgebra,Test, DomainSets
+using ModelingToolkit, MethodOfLines, DiffEqBase, LinearAlgebra, Test, DomainSets
 #using Plots
 # Tests
 
@@ -13,44 +13,44 @@ using ModelingToolkit,MethodOfLines,DiffEqBase,LinearAlgebra,Test, DomainSets
     Dx = Differential(x)
 
     # 1D PDE and boundary conditions
-    eq  = Dt(u(t,x)) ~ -Dx(u(t,x))
+    eq = Dt(u(t, x)) ~ -Dx(u(t, x))
 
-    asf(x) = (0.5/(0.2*sqrt(2.0*3.1415)))*exp(-(x-1.0)^2/(2.0*0.2^2))
-    bcs = [u(0,x) ~ asf(x),
-    u(t,0) ~ u(t,2)]
-    
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2)]
+
     # Space and time domains
-    domains = [t ∈ Interval(0.0,2.0),
-               x ∈ Interval(0.0,2.0)]
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
 
     # PDE system
-    @named pdesys = PDESystem(eq,bcs,domains,[t,x],[u(t,x)])
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x)])
 
     # Method of lines discretization
-    dx = 2/80
+    dx = 2 / 80
     order = 1
-    discretization = MOLFiniteDifference([x=>dx],t)
+    discretization = MOLFiniteDifference([x => dx], t)
     # explicitly specify upwind order
-    discretization_upwind = MOLFiniteDifference([x=>dx], t; upwind_order=order)
+    discretization_upwind = MOLFiniteDifference([x => dx], t; upwind_order=order)
 
     # Convert the PDE problem into an ODE problem
-    prob = discretize(pdesys,discretization)
+    prob = discretize(pdesys, discretization)
     prob_upwind = discretize(pdesys, discretization_upwind)
 
     # Solve ODE problem
     using OrdinaryDiffEq
-    sol = solve(prob,Euler(),dt=.025,saveat=0.1)
-    sol_upwind = solve(prob_upwind,Euler(),dt=.025,saveat=0.1)
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
+    sol_upwind = solve(prob_upwind, Euler(), dt=0.025, saveat=0.1)
 
     x_interval = infimum(domains[2].domain)+dx:dx:supremum(domains[2].domain)
     u = asf.(x_interval)
     # Plot and save results
 
     # Test
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
 
     x_sol = x_interval
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
     exact = u
     # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
     # plot!(x_sol, sol.u[end], label="Numeric solution")
@@ -59,8 +59,8 @@ using ModelingToolkit,MethodOfLines,DiffEqBase,LinearAlgebra,Test, DomainSets
     # savefig("plots/MOL_Linear_Convection_Test00.png")
 
 
-    @test sol.u[end] ≈ u atol = 0.1;
-    @test sol_upwind.u[end] ≈ u atol = 0.1;
+    @test sol.u[end] ≈ u atol = 0.1
+    @test sol_upwind.u[end] ≈ u atol = 0.1
 end
 
 @testset "Test 00a: Dt(u(t,x)) ~ Dx(u(t,x))" begin
@@ -71,44 +71,44 @@ end
     Dx = Differential(x)
 
     # 1D PDE and boundary conditions
-    eq  = Dt(u(t,x)) ~ -Dx(u(t,x))
+    eq = Dt(u(t, x)) ~ Dx(u(t, x))
 
-    asf(x) = (0.5/(0.2*sqrt(2.0*3.1415)))*exp(-(x-1.0)^2/(2.0*0.2^2))
-    bcs = [u(0,x) ~ asf(x),
-    u(t,0) ~ u(t,2)]
-    
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2)]
+
     # Space and time domains
-    domains = [t ∈ Interval(0.0,2.0),
-               x ∈ Interval(0.0,2.0)]
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
 
     # PDE system
-    @named pdesys = PDESystem(eq,bcs,domains,[t,x],[u(t,x)])
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x)])
 
     # Method of lines discretization
-    dx = 2/80
+    dx = 2 / 80
     order = 1
-    discretization = MOLFiniteDifference([x=>dx],t)
+    discretization = MOLFiniteDifference([x => dx], t)
     # explicitly specify upwind order
-    discretization_upwind = MOLFiniteDifference([x=>dx], t; upwind_order=order)
+    discretization_upwind = MOLFiniteDifference([x => dx], t; upwind_order=order)
 
     # Convert the PDE problem into an ODE problem
-    prob = discretize(pdesys,discretization)
+    prob = discretize(pdesys, discretization)
     prob_upwind = discretize(pdesys, discretization_upwind)
 
     # Solve ODE problem
     using OrdinaryDiffEq
-    sol = solve(prob,Euler(),dt=.025,saveat=0.1)
-    sol_upwind = solve(prob_upwind,Euler(),dt=.025,saveat=0.1)
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
+    sol_upwind = solve(prob_upwind, Euler(), dt=0.025, saveat=0.1)
 
     x_interval = infimum(domains[2].domain)+dx:dx:supremum(domains[2].domain)
     u = asf.(x_interval)
     # Plot and save results
 
     # Test
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
 
     x_sol = x_interval
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
     exact = u
     # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
     # plot!(x_sol, sol.u[end], label="Numeric solution")
@@ -117,8 +117,124 @@ end
     # savefig("plots/MOL_Linear_Convection_Test00.png")
 
 
-    @test sol.u[end] ≈ u atol = 0.1;
-    @test sol_upwind.u[end] ≈ u atol = 0.1;
+    @test sol.u[end] ≈ u atol = 0.1
+    @test sol_upwind.u[end] ≈ u atol = 0.1
+end
+
+@testset "Test 00b: Dt(u(t,x)) - Dx(u(t,x)) ~ 0" begin
+    # Parameters, variables, and derivatives
+    @parameters t x
+    @variables u(..)
+    Dt = Differential(t)
+    Dx = Differential(x)
+
+    # 1D PDE and boundary conditions
+    eq = Dt(u(t, x)) - Dx(u(t, x)) ~ 0
+
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2)]
+
+    # Space and time domains
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
+
+    # PDE system
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x)])
+
+    # Method of lines discretization
+    dx = 2 / 80
+    order = 1
+    discretization = MOLFiniteDifference([x => dx], t)
+    # explicitly specify upwind order
+    discretization_upwind = MOLFiniteDifference([x => dx], t; upwind_order=order)
+
+    # Convert the PDE problem into an ODE problem
+    prob = discretize(pdesys, discretization)
+    prob_upwind = discretize(pdesys, discretization_upwind)
+
+    # Solve ODE problem
+    using OrdinaryDiffEq
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
+    sol_upwind = solve(prob_upwind, Euler(), dt=0.025, saveat=0.1)
+
+    x_interval = infimum(domains[2].domain)+dx:dx:supremum(domains[2].domain)
+    u = asf.(x_interval)
+    # Plot and save results
+
+    # Test
+    t_f = size(sol, 3)
+
+    x_sol = x_interval
+    t_f = size(sol, 3)
+    exact = u
+    # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
+    # plot!(x_sol, sol.u[end], label="Numeric solution")
+    # plot!(x_sol, u.-sol.u[end], label="Differential Error")
+
+    # savefig("plots/MOL_Linear_Convection_Test00.png")
+
+
+    @test_broken sol.u[end] ≈ u atol = 0.1
+    @test_broken sol_upwind.u[end] ≈ u atol = 0.1
+end
+
+@testset "Test 00c: Dt(u(t,x)) + Dx(u(t,x)) ~ 0" begin
+    # Parameters, variables, and derivatives
+    @parameters t x
+    @variables u(..)
+    Dt = Differential(t)
+    Dx = Differential(x)
+
+    # 1D PDE and boundary conditions
+    eq = Dt(u(t, x)) - Dx(u(t, x)) ~ 0
+
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2)]
+
+    # Space and time domains
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
+
+    # PDE system
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x)])
+
+    # Method of lines discretization
+    dx = 2 / 80
+    order = 1
+    discretization = MOLFiniteDifference([x => dx], t)
+    # explicitly specify upwind order
+    discretization_upwind = MOLFiniteDifference([x => dx], t; upwind_order=order)
+
+    # Convert the PDE problem into an ODE problem
+    prob = discretize(pdesys, discretization)
+    prob_upwind = discretize(pdesys, discretization_upwind)
+
+    # Solve ODE problem
+    using OrdinaryDiffEq
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
+    sol_upwind = solve(prob_upwind, Euler(), dt=0.025, saveat=0.1)
+
+    x_interval = infimum(domains[2].domain)+dx:dx:supremum(domains[2].domain)
+    u = asf.(x_interval)
+    # Plot and save results
+
+    # Test
+    t_f = size(sol, 3)
+
+    x_sol = x_interval
+    t_f = size(sol, 3)
+    exact = u
+    # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
+    # plot!(x_sol, sol.u[end], label="Numeric solution")
+    # plot!(x_sol, u.-sol.u[end], label="Differential Error")
+
+    # savefig("plots/MOL_Linear_Convection_Test00.png")
+
+
+    @test_broken sol.u[end] ≈ u atol = 0.1
+    @test_broken sol_upwind.u[end] ≈ u atol = 0.1
 end
 
 @testset "Test 01: Dt(u(t,x)) ~ -Dx(u(t,x)) + 0.001" begin
@@ -130,29 +246,29 @@ end
     Dx = Differential(x)
 
     # 1D PDE and boundary conditions
-    eq  = Dt(u(t,x)) ~ -Dx(u(t,x)) + 0.001
-    asf(x) = (0.5/(0.2*sqrt(2.0*3.1415)))*exp(-(x-1.0)^2/(2.0*0.2^2))
-    bcs = [u(0,x) ~ asf(x),
-           u(t,0) ~ u(t,2)]
-    
+    eq = Dt(u(t, x)) ~ -Dx(u(t, x)) + 0.001
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2)]
+
     # Space and time domains
-    domains = [t ∈ Interval(0.0,2.0),
-               x ∈ Interval(0.0,2.0)]
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
 
     # PDE system
-    @named pdesys = PDESystem(eq,bcs,domains,[t,x],[u(t,x)])
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x)])
 
     # Method of lines discretization
-    dx = 2/80
+    dx = 2 / 80
     order = 1
-    discretization = MOLFiniteDifference([x=>dx],t)
+    discretization = MOLFiniteDifference([x => dx], t)
 
     # Convert the PDE problem into an ODE problem
-    prob = discretize(pdesys,discretization)
+    prob = discretize(pdesys, discretization)
 
     # Solve ODE problem
     using OrdinaryDiffEq
-    sol = solve(prob,Euler(),dt=.025,saveat=0.1)
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
 
 
     # Test
@@ -161,10 +277,10 @@ end
     # Plot and save results
 
     # Test
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
 
     x_sol = x_interval
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
     exact = u
     # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
     # plot!(x_sol, sol.u[end], label="Numeric solution")
@@ -173,7 +289,7 @@ end
     # savefig("plots/MOL_Linear_Convection_Test01.png")
 
 
-    @test sol.u[end] ≈ u atol = 0.1;
+    @test sol.u[end] ≈ u atol = 0.1
 
 end
 
@@ -187,29 +303,29 @@ end
     v = 1.0
 
     # 1D PDE and boundary conditions
-    eq  = Dt(u(t,x)) ~ -v*Dx(u(t,x))
-    asf(x) = (0.5/(0.2*sqrt(2.0*3.1415)))*exp(-(x-1.0)^2/(2.0*0.2^2))
-    bcs = [u(0,x) ~ asf(x),
-           u(t,0) ~ u(t,2)]
-    
+    eq = Dt(u(t, x)) ~ -v * Dx(u(t, x))
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2)]
+
     # Space and time domains
-    domains = [t ∈ Interval(0.0,2.0),
-               x ∈ Interval(0.0,2.0)]
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
 
     # PDE system
-    @named pdesys = PDESystem(eq,bcs,domains,[t,x],[u(t,x)])
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x)])
 
     # Method of lines discretization
-    dx = 2/80
+    dx = 2 / 80
     order = 1
-    discretization = MOLFiniteDifference([x=>dx],t, upwind_order = 1)
+    discretization = MOLFiniteDifference([x => dx], t, upwind_order=1)
 
     # Convert the PDE problem into an ODE problem
-    prob = discretize(pdesys,discretization)
+    prob = discretize(pdesys, discretization)
 
     # Solve ODE problemdoes this mean that - doesn't seem to affect things
     using OrdinaryDiffEq
-    sol = solve(prob,Euler(),dt=.025,saveat=0.1)
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
 
     # Plot and save results
     # using Plots
@@ -227,10 +343,10 @@ end
     # Plot and save results
 
     # Test
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
 
     x_sol = x_interval
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
     exact = u
     # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
     # plot!(x_sol, sol.u[end], label="Numeric solution")
@@ -245,8 +361,8 @@ end
     #     plot!(x_sol, sol.u[i], label="Numeric solution at $(sol.t[i])")
     # end
     # gif(anim, "plots/MOL_Linear_Convection_Test02.gif", fps = 5)
-    
-    @test sol.u[end] ≈ u atol = 0.1;
+
+    @test sol.u[end] ≈ u atol = 0.1
 end
 @test_broken begin#@testset "Test 03: Dt(u(t,x)) ~ -Dx(v(t,x))*u(t,x)-v(t,x)*Dx(u(t,x)) with v(t,x)=1" begin
     # Parameters, variables, and derivatives
@@ -256,38 +372,38 @@ end
     Dx = Differential(x)
 
     # 1D PDE and boundary conditions
-    eq  = [ Dt(u(t,x)) ~ -(Dx(v(t,x))*u(t,x) + v(t,x)*Dx(u(t,x))),
-            v(t,x) ~ 1.0 ]
-    asf(x) = (0.5/(0.2*sqrt(2.0*3.1415)))*exp(-(x-1.0)^2/(2.0*0.2^2))
-    bcs = [u(0,x) ~ asf(x),
-           u(t,0) ~ u(t,2),
-           v(0,x) ~ 1.0,
-           v(t,0) ~ 1.0,
-           v(t,2) ~ 1.0
-           ]
+    eq = [Dt(u(t, x)) ~ -(Dx(v(t, x)) * u(t, x) + v(t, x) * Dx(u(t, x))),
+        v(t, x) ~ 1.0]
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2),
+        v(0, x) ~ 1.0,
+        v(t, 0) ~ 1.0,
+        v(t, 2) ~ 1.0
+    ]
 
     # Space and time domains
 
-    
+
     # Space and time domains
-    domains = [t ∈ Interval(0.0,2.0),
-               x ∈ Interval(0.0,2.0)]
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
 
 
     # PDE system
-    @named pdesys = PDESystem(eq,bcs,domains,[t,x],[u(t,x),v(t,x)])
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x), v(t, x)])
 
     # Method of lines discretization
-    dx = 2/80
+    dx = 2 / 80
     order = 1
-    discretization = MOLFiniteDifference([x=>dx],t)
+    discretization = MOLFiniteDifference([x => dx], t)
 
     # Convert the PDE problem into an ODE problem
-    prob = discretize(pdesys,discretization)
+    prob = discretize(pdesys, discretization)
 
     # Solve ODE problem
     using OrdinaryDiffEq
-    sol = solve(prob,Euler(),dt=.025,saveat=0.1)
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
 
     #Plot and save results
     # using Plots
@@ -305,10 +421,10 @@ end
     # Plot and save results
 
     # Test
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
 
     x_sol = x_interval
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
     exact = u
     # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
     # plot!(x_sol, sol.u[end], label="Numeric solution")
@@ -317,7 +433,7 @@ end
     # savefig("plots/MOL_Linear_Convection_Test03.png")
 
 
-    @test sol.u[end] ≈ u atol = 0.1;
+    @test sol.u[end] ≈ u atol = 0.1
 end
 
 @testset "Test 04: Dt(u(t,x)) ~ -Dx(v(t,x))*u(t,x)-v(t,x)*Dx(u(t,x)) with v(t,x)=0.999 + 0.001 * t * x " begin
@@ -328,34 +444,34 @@ end
     Dx = Differential(x)
 
     # 1D PDE and boundary conditions
-    eq  = [ Dt(u(t,x)) ~ -Dx(v(t,x))*u(t,x)-v(t,x)*Dx(u(t,x)),
-            v(t,x) ~ 0.999 + 0.001 * t * x ]
-    asf(x) = (0.5/(0.2*sqrt(2.0*3.1415)))*exp(-(x-1.0)^2/(2.0*0.2^2))
-    
-    bcs = [u(0,x) ~ asf(x),
-           u(t,0) ~ u(t,2),
-           v(0,x) ~ 0.999,
-           v(t,0) ~ 0.999,
-           v(t,2) ~ 0.9999 + 0.0001 * t * 2.0]
+    eq = [Dt(u(t, x)) ~ -Dx(v(t, x)) * u(t, x) - v(t, x) * Dx(u(t, x)),
+        v(t, x) ~ 0.999 + 0.001 * t * x]
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2),
+        v(0, x) ~ 0.999,
+        v(t, 0) ~ 0.999,
+        v(t, 2) ~ 0.9999 + 0.0001 * t * 2.0]
 
     # Space and time domains
-    domains = [t ∈ Interval(0.0,2.0),
-               x ∈ Interval(0.0,2.0)]
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
 
     # PDE system
-    @named pdesys = PDESystem(eq,bcs,domains,[t,x],[u(t,x),v(t,x)])
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x), v(t, x)])
 
     # Method of lines discretization
-    dx = 2/80
+    dx = 2 / 80
     order = 1
-    discretization = MOLFiniteDifference([x=>dx],t)
+    discretization = MOLFiniteDifference([x => dx], t)
 
     # Convert the PDE problem into an ODE problem
-    prob = discretize(pdesys,discretization)
+    prob = discretize(pdesys, discretization)
 
     # Solve ODE problem
     using OrdinaryDiffEq
-    sol = solve(prob,Euler(),dt=.025,saveat=0.1)
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
 
     # Plot and save results
     # using Plots
@@ -373,10 +489,10 @@ end
     # Plot and save results
 
     # Test
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
 
     x_sol = x_interval
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
     exact = u
     # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
     # plot!(x_sol, sol.u[end], label="Numeric solution")
@@ -389,10 +505,10 @@ end
     #     plot!(x_sol, sol.u[i], label="Numeric solution at $(sol.t[i])")
     # end
     # gif(anim, "plots/MOL_Linear_Convection_Test04.gif", fps = 5)
-    
 
 
-    @test sol.u[end] ≈ u atol = 0.1;
+
+    @test sol.u[end] ≈ u atol = 0.1
 end
 
 
@@ -404,34 +520,34 @@ end
     Dx = Differential(x)
 
     # 1D PDE and boundary conditions
-    eq  = [ Dt(u(t,x)) ~ -Dx(v(t,x)*u(t,x)),
-            v(t,x) ~ 0.999 + 0.001 * t * x ]
-    asf(x) = (0.5/(0.2*sqrt(2.0*3.1415)))*exp(-(x-1.0)^2/(2.0*0.2^2))
-    
-    bcs = [u(0,x) ~ asf(x),
-           u(t,0) ~ u(t,2),
-           v(0,x) ~ 0.999,
-           v(t,0) ~ 0.999,
-           v(t,2) ~ 0.9999 + 0.0001 * t * 2.0]
+    eq = [Dt(u(t, x)) ~ -Dx(v(t, x) * u(t, x)),
+        v(t, x) ~ 0.999 + 0.001 * t * x]
+    asf(x) = (0.5 / (0.2 * sqrt(2.0 * 3.1415))) * exp(-(x - 1.0)^2 / (2.0 * 0.2^2))
+
+    bcs = [u(0, x) ~ asf(x),
+        u(t, 0) ~ u(t, 2),
+        v(0, x) ~ 0.999,
+        v(t, 0) ~ 0.999,
+        v(t, 2) ~ 0.9999 + 0.0001 * t * 2.0]
 
     # Space and time domains
-    domains = [t ∈ Interval(0.0,2.0),
-               x ∈ Interval(0.0,2.0)]
+    domains = [t ∈ Interval(0.0, 2.0),
+        x ∈ Interval(0.0, 2.0)]
 
     # PDE system
-    @named pdesys = PDESystem(eq,bcs,domains,[t,x],[u(t,x),v(t,x)])
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x), v(t, x)])
 
     # Method of lines discretization
-    dx = 2/80
+    dx = 2 / 80
     order = 1
-    discretization = MOLFiniteDifference([x=>dx],t)
+    discretization = MOLFiniteDifference([x => dx], t)
 
     # Convert the PDE problem into an ODE problem
-    prob = discretize(pdesys,discretization)
+    prob = discretize(pdesys, discretization)
 
     # Solve ODE problem
     using OrdinaryDiffEq
-    sol = solve(prob,Euler(),dt=.025,saveat=0.1)
+    sol = solve(prob, Euler(), dt=0.025, saveat=0.1)
 
     # Plot and save results
     # using Plots
@@ -449,10 +565,10 @@ end
     # Plot and save results
 
     # Test
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
 
     x_sol = x_interval
-    t_f = size(sol,3)
+    t_f = size(sol, 3)
     exact = u
     # plot(x_sol, u, seriestype = :scatter,label="Analytic solution")
     # plot!(x_sol, sol.u[end], label="Numeric solution")
@@ -465,9 +581,8 @@ end
     #     plot!(x_sol, sol.u[i], label="Numeric solution at $(sol.t[i])")
     # end
     # gif(anim, "plots/MOL_Linear_Convection_Test04.gif", fps = 5)
-    
 
 
-    @test sol.u[end] ≈ u atol = 0.1;
+
+    @test sol.u[end] ≈ u atol = 0.1
 end
-
