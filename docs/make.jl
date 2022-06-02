f5a93ff7ad30b209c99f215d627485122ebd1f79
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-06-02 07:54:11 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -15,27 +15,7 @@ makedocs(
     ],
     modules=[MethodOfLines], format=Documenter.HTML(analytics="UA-90474609-3",
         assets=["assets/favicon.ico"],
-        canonical="https://methodoflines.sciml.ai/stable/"), pages=[
-        "MethodOfLines.jl: Automated Finite Difference for Phyiscs-Informed Learning" => "index.md",
-        "Tutorials" => ["tutorials/brusselator.md", "tutorials/heat.md", "tutorials/params.md", "tutorials/heatss.md", "tutorials/icbc_sampled.md"],
-        "MOLFiniteDifference" => "MOLFiniteDifference.md",
-        "Grid and Solution Retrieval" => "get_grid.md",
-        "Boundary Conditions" => "boundary_conditions.md",
-        "Non-Uniform Rectilinear Grids" => "nonuniform.md",
-        "Curvilinear Grids" => "curvilinear_grids.md",
-        "How it Works" => "howitworks.md",
-        "Notes for Developers: Implement a Scheme" => "devnotes.md",
-        "Generated Examples" => ["generated/bruss_code.md", "generated/bruss_ode_eqs.md"]
-        # "Tutorial: Burgers" => "tutorials/burgers.md",
-        # "Tutorial: 1D Linear Diffusion" => "tutorials/1d_linear_diffusion.md",
-        # "Tutorial: 1D Non-Linear Diffusion" => "tutorials/1d_nonlinear_diffusion.md",
-        # "Tutorial: 2D Diffusion" => "tutorials/2d_diffusion.md",
-        # "Tutorial: 1D Linear Convection" => "tutorials/1d_linear_convection.md",
-        # "Tutorial: 1D Higher Order" => "tutorials/1d_higher_order.md",
-        # "Tutorial: Stationary Nonlinear Problems" => "tutorials/stationary_nonlinear_problems.md",
-        # "Tutorial: 1D Partial DAE" => "tutorials/1d_partial_DAE.md",
-
-    ]
+        canonical="https://methodoflines.sciml.ai/stable/"), pages=pages
 )
 
 deploydocs(
