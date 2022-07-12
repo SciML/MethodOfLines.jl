4c691fa905c8263dc82a93b4d0a7c0691171b3a5
xtalax <alex.lemony@gmail.com>
2022-07-12 22:10:09 +0100

    using Random

--- test/pde_systems/MOL_1D_NonLinear_Diffusion_NonUniform.jl
+++ test/pde_systems/MOL_1D_NonLinear_Diffusion_NonUniform.jl
@@ -6,6 +6,8 @@
 # Packages and inclusions
 using ModelingToolkit, MethodOfLines, LinearAlgebra, Test, OrdinaryDiffEq, DomainSets
 using ModelingToolkit: Differential
+using Random
+
 # Tests
 @testset "Test 00: Dt(u(t,x)) ~ Dx(u(t,x)^(-1) * Dx(u(t,x)))" begin
     # Variables, parameters, and derivatives
