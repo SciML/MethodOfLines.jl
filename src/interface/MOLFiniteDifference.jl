c3aecb44962296fe32afb088f1ebb7347cb52585
xtalax <alex.lemony@gmail.com>
2022-10-14 15:59:04 +0100

    fix interface

--- src/interface/MOLFiniteDifference.jl
+++ src/interface/MOLFiniteDifference.jl
@@ -44,5 +44,5 @@ function MOLFiniteDifference(dxs, time=nothing; approx_order = 2, advection_sche
 
     @assert (time isa Num) | (time isa Nothing) "time must be a Num, or Nothing - got $(typeof(time)). See docs for MOLFiniteDifference."
 
-    return MOLFiniteDifference{typeof(grid_align)}(dxs, time, approx_order, advection_scheme, grid_align, kwargs)
+    return MOLFiniteDifference{typeof(grid_align)}(dxs, time, approx_order, advection_scheme, grid_align, should_transform, use_ODAE, kwargs)
 end
