617e4f0d8edfdd96e0fdccb6d7d42d6dd347c4a0
xtalax <alex.lemony@gmail.com>
2022-10-28 15:23:49 +0100

    no mutation

--- src/system_parsing/pde_system_transformation.jl
+++ src/system_parsing/pde_system_transformation.jl
@@ -5,8 +5,8 @@ Modified copilot explanation:
 
 """
 function transform_pde_system!(v, boundarymap, pmap, sys::PDESystem)
-    eqs = sys.eqs
-    bcs = sys.bcs
+    eqs = copy(sys.eqs)
+    bcs = copy(sys.bcs)
     done = false
     # Replace bad terms with a greedy strategy until the system comes up clean
     while !done
