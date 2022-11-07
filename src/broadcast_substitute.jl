3855972a13df2de701d586e431bac83774b9b15c
xtalax <alex.lemony@gmail.com>
2022-11-07 16:39:57 +0000

    broadcast substitute

---
+++ src/broadcast_substitute.jl
@@ -0,0 +1,21 @@
+broadcast_substitute(expr, dict) = _sub(expr, dict isa Dict: dict : Dict(dict))
+
+function _sub(expr, dict)
+    if haskey(dict, expr)
+        return dict[expr]
+    elseif istree(expr)
+        op = operation(expr)
+        args = sub.(arguments(expr), (dict,))
+        if any(arg -> symtype(arg) <: AbstractArray, args)
+            try
+                return unwrap(op(map(wrap, args)...))
+            catch e
+                throw(ArgumentError("Cannot broadcast operation $op over arguments $args"))
+            end
+        else
+            return op(args...)
+        end
+    else
+        return expr
+    end
+end
