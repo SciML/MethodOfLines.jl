a2654d9602515a098987323c294fc2b35ffa4837
qfl3x <qfl3x@protonmail.com>
2022-11-29 11:26:02 +0100

    Changes, not working yet

--- src/discretization/schemes/centered_difference/centered_difference.jl
+++ src/discretization/schemes/centered_difference/centered_difference.jl
@@ -23,7 +23,7 @@ function central_difference(D::DerivativeOperator{T,N,Wind,DX}, II, s, bs, jx, u
         Itap = [bwrap(II + i * I1, bs, s, jx) for i in half_range(D.stencil_length)]
     end
     # Tap points of the stencil, this uses boundary_point_count as this is equal to half the stencil size, which is what we want.
-
+    @info "CENTER"
     return dot(weights, ufunc(u, Itap, x))
 end
 
