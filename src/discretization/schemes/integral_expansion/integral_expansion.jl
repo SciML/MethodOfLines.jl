4f0ef61e55bfa8e53d76611120a2e72e556d8bc9
xtalax <alex.lemony@gmail.com>
2022-12-13 15:53:13 +0000

    support noinuniform

--- src/discretization/schemes/integral_expansion/integral_expansion.jl
+++ src/discretization/schemes/integral_expansion/integral_expansion.jl
@@ -1,15 +1,35 @@
-function euler_integral(II, s, jx, u, ufunc) #where {T,N,Wind,DX<:Number}
+#TODO: Add support for non-uniform dx
+#TODO: Use the rectangle rule, trapezium rule and simpsons rule to approximate the integral.
+function _euler_integral(II, s, jx, u, ufunc, dx::Number) #where {T,N,Wind,DX<:Number}
     j, x = jx
     ndims(u, s) != 1 && return Num(0)
     # unit index in direction of the derivative
     I1 = unitindex(ndims(u, s), j)
     # dx for multiplication
-    dx = s.dxs[x]
 
     Itap = [(II - I1 * i) for i = 0:(II[j]-1)] # Sum from current position till CartesianIndex 2
     weights = [dx * (i != (II[j] - 1)) for i = 0:(II[j]-1)]
 
     return dot(weights, ufunc(u, Itap, x))
+end
+
+# Nonuniform dx
+function _euler_integral(II, s, jx, u, ufunc, dx::AbstractVector) #where {T,N,Wind,DX<:Number}
+    j, x = jx
+    ndims(u, s) != 1 && return Num(0)
+    # unit index in direction of the derivative
+    I1 = unitindex(ndims(u, s), j)
+    # dx for multiplication
+
+    Itap = [(II - I1 * i) for i = (II[j]-1):-1:0] # Sum from current position till CartesianIndex 2
+    weights = [dx[i+1] * (i != (II[j] - 1)) for i = 0:(II[j]-1)]
+
+    return dot(weights, ufunc(u, Itap, x))
+end
+
+function euler_itegral(II, s, jx, u, ufunc)
+    dx = s.dxs[jx[2]]
+    return _euler_integral(II, s, jx, u, ufunc, dx)
 end
 
 @inline function generate_integration_rules(II::CartesianIndex, s::DiscreteSpace, depvars, indexmap, terms)
