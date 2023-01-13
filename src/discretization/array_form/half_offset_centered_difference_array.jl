578ab54641e6fe0aeef0f8e94c6ac7f2a7ffaa20
xtalax <alex.lemony@gmail.com>
2023-01-13 15:19:15 +0000

    stencilize laplacians

--- src/discretization/array_form/half_offset_centered_difference_array.jl
+++ src/discretization/array_form/half_offset_centered_difference_array.jl
@@ -2,8 +2,7 @@
 # Stencil interface
 ########################################################################################
 
-function half_offset_centered_difference(D::DerivativeOperator, interior, s, b, jx, u, udisc, len)
-    args = params(u, s)
+function half_offset_centered_difference(D::DerivativeOperator, interior, s, bs, jx, u, udisc, len)|
     interior = get_interior(u, s, interior)
     is = get_is(u, s)
 
@@ -11,22 +10,25 @@ function half_offset_centered_difference(D::DerivativeOperator, interior, s, b, 
     offset = len == 0 ? 0 : -1
     lenx = len == 0 ? length(s, x) : len
 
-    if b isa Val{false}
+    haslower, hasupper = haslowerupper(bs, x)
+
+    lowerops = []
+    upperops = []
+
+    if !haslower
         lowerops = map(interior[j][1]:D.boundary_point_count) do iboundary
             lower_boundary_deriv(D, udisc, iboundary, j, is, interior)
         end
-
-        upperops = map((lenx-D.boundary_point_count+1):(interior[j][end]+offset)) do iboundary
+    end
+    if !hasupper
+        upperops = map((lenx-D.boundary_point_count+1):interior[j][end]+offset) do iboundary
             upper_boundary_deriv(D, udisc, iboundary, j, is, interior, lenx)
         end
-    else
-        lowerops = []
-        upperops = []
     end
 
-    interiorop = interior_deriv(D, udisc,
-                                (1-div(D.stencil_length, 2)):(div(D.stencil_length, 2)),
-                                j, is, interior, b)
+    interiorop = interior_deriv(D, udisc, s,
+        (1-div(D.stencil_length, 2)):(div(D.stencil_length, 2)),
+        j, is, interior, bs)
     boundaryoppairs = vcat(lowerops, upperops)
 
     return Construct_ArrayMaker(interior, vcat(Tuple(interior) => interiorop, boundaryoppairs))
---
+++ src/discretization/schemes/nonlinear_laplacian/nonlinear_laplacian_array.jl
@@ -0,0 +1,59 @@
+########################################################################################
+# Stencil interface
+########################################################################################
+
+#TODO: Decouple this from old implementation
+
+function cartesian_nonlinear_laplacian(expr, interior, derivweights, s::DiscreteSpace{N}, bs, depvars, x, u) where {N}
+    args = params(u, s)
+    interior = get_interior(u, s, interior)
+    is = get_is(u, s)
+
+    N = ndims(u, s)
+    N == 0 && return Num(0)
+    jx = j, x = (x2i(s, u, x), x)
+
+    D_inner = derivweights.halfoffsetmap[1][Differential(x)]
+    D_outer = derivweights.halfoffsetmap[2][Differential(x)]
+    inner_interpolater = derivweights.interpmap[x]
+
+    # Get the outer weights and stencil. clip() essentially removes a point from either end of the grid, for this reason this function is only defined on the interior, not in bcs
+    #* Need to see how to handle this with interface boundaries
+
+    udisc = s.discvars[u]
+    lenx = length(s, x)
+    cliplen = lenx - 1
+
+    innerderiv = half_offset_centered_difference(D_inner, interior, s, bs, jx, u, udisc, len)
+
+    interpvars = [v => half_offset_centered_difference(inner_interpolater, interior, s, bs, jx, v, s.discvars[v], len)]
+    interpparams = map(xpair -> xpair.first => half_offset_centered_difference(inner_interpolater, interior, s, bs, jx, x, xpair.second, len), gridvals(s, u, interior))
+
+    interpolated_expr = broadcast_substitute(expr, vcat(interpvars, interpparams))
+
+    inner_arg = interpolated_expr .* innerderiv
+
+    outerderiv = half_offset_centered_difference(D_outer, interior, s, bs, jx, u, inner_arg, cliplen)
+
+    return outerderiv
+end
+
+@inline function generate_nonlinlap_rules(interior, s::DiscreteSpace, depvars, derivweights::DifferentialDiscretizer, pmap, indexmap, terms)
+    rules = reduce(safe_vcat, [vec([@rule *(~~c, $(Differential(x))(*(~~a, $(Differential(x))(u), ~~b)), ~~d) => *(~c..., cartesian_nonlinear_laplacian(*(a..., b...), interior, derivweights, s, pmap.map[operation(u)][x], depvars, x, u), ~d...) for x in params(u, s)]) for u in depvars], init = [])
+
+    rules = safe_vcat(rules, reduce(safe_vcat, [vec([@rule $(Differential(x))(*(~~a, $(Differential(x))(u), ~~b)) => cartesian_nonlinear_laplacian(*(a..., b...), interior, derivweights, s, pmap.map[operation(u)][x], depvars, x, u) for x in params(u, s)]) for u in depvars], init = []))
+
+    rules = safe_vcat(rules, reduce(safe_vcat, [vec([@rule ($(Differential(x))($(Differential(x))(u) / ~a)) => cartesian_nonlinear_laplacian(1 / ~a, interior, derivweights, s, pmap.map[operation(u)][x], depvars, x, u) for x in params(u, s)]) for u in depvars], init = []))
+
+    rules = safe_vcat(rules, reduce(safe_vcat, [vec([@rule *(~~b, ($(Differential(x))($(Differential(x))(u) / ~a)), ~~c) => *(b..., c..., cartesian_nonlinear_laplacian(1 / ~a, interior, derivweights, s, pmap.map[operation(u)][x], depvars, x, u)) for x in params(u, s)]) for u in depvars], init = []))
+
+    nonlinlap_rules = []
+    for t in terms
+        for r in rules
+            if r(t) !== nothing
+                push!(nonlinlap_rules, t => r(t))
+            end
+        end
+    end
+    return nonlinlap_rules
+end
