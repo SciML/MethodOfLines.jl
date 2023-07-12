d642f1a7bba1b1bd5b27a87e78064f88a27a7542
Criston Hyett <cmhyett@math.arizona.edu>
2023-07-12 12:40:45 -0700

    implementing centered finite difference for staggered grid\!

--- examples/waveEquation.jl
+++ examples/waveEquation.jl
@@ -5,15 +5,17 @@ using OrdinaryDiffEq, ModelingToolkit, MethodOfLines, DomainSets, Plots;
 Dt = Differential(t);
 Dx = Differential(x);
 
-a = 0.1;
+a = 0.125;
 L = 3.0;
-dx = 1.0;
+dx = 0.25;
 
-initialFunction(x) = relu(x-L/4) - 2*relu(x-(2*L/4)) + relu(x-(3*L/4))
+relu(x) = max(x,0);
+#initialFunction(x) = relu(x-L/4) - 2*relu(x-(2*L/4)) + relu(x-(3*L/4))
+initialFunction(x) = relu(x-6*L/8) - 2*relu(x-(7*L/8));
 
 eq = [Dt(ρ(t,x)) + Dx(ϕ(t,x)) ~ 0,
       Dt(ϕ(t,x)) + a^2 * Dx(ρ(t,x)) ~ 0]
-bcs = [ρ(0.0,x) ~ exp(-(x-L/2)^2),
+bcs = [ρ(0.0,x) ~ initialFunction(x),
        ϕ(0.0,x) ~ 0.0,
        ρ(t,0) ~ ρ(t,L),
        ϕ(t,0) ~ ϕ(t,L)];
--- src/discretization/schemes/centered_difference/centered_difference.jl
+++ src/discretization/schemes/centered_difference/centered_difference.jl
@@ -32,7 +32,6 @@ function central_difference(D::DerivativeOperator{T,N,Wind,DX}, II, s, bs, jx, u
     ndims(u, s) == 0 && return 0
     # unit index in direction of the derivative
     I1 = unitindex(ndims(u, s), j)
-    # offset is important due to boundary proximity
 
     if (II[j] <= D.boundary_point_count)
         weights = D.low_boundary_coefs[II[j]]
@@ -61,4 +60,51 @@ This is a catch all ruleset, as such it does not use @rule. Any even ordered der
             orders[iseven.(orders)]
         end
     )] for x in ivs(u, s)], init = []) for u in depvars], init = [])
+end
+
+
+function generate_cartesian_rules(II::CartesianIndex, s::DiscreteSpace{N,M,G}, depvars, derivweights::DifferentialDiscretizer, bcmap, indexmap, terms) where {N,M,G<:StaggeredGrid}
+    central_ufunc(u, I, x) = s.discvars[u][I]
+    xs = unique(reduce(safe_vcat, [ivs(u, s) for u in depvars], init=[]));
+    odd_orders = unique(filter(isodd, reduce(safe_vcat, [derivweights.orders[x] for x in xs], init=[])));
+    placeholder = [reduce(safe_vcat,
+                          [[(Differential(x)^d)(u) =>
+                              central_difference(derivweights.map[Differential(x)^d],
+                                                 Idx(II, s, u, indexmap),
+                                                 s,
+                                                 filter_interfaces(bcmap[operation(u)][x]),
+                                                 (x2i(s, u, x), x),
+                                                 u,
+                                                 central_ufunc)
+                            for d in odd_orders] for x in xs], init = []) for u in depvars];
+    return reduce(safe_vcat, placeholder, init = [])
+end
+    
+function central_difference(D::DerivativeOperator{T,N,Wind,DX}, II, s::DiscreteSpace{W,M,G}, bs, jx, u, ufunc) where {T,N,Wind,DX<:Number,W,M,G<:StaggeredGrid}
+    j, x = jx
+    ndims(u, s) == 0 && return 0
+    # unit index in direction of the derivative
+    I1 = unitindex(ndims(u, s), j)
+    staggered_range(len, is_staggered) = !is_staggered ? (len-1:-1:0) : (0:-1:-len+1);
+    is_staggered = isequal(u, s.vars.ū[2]) ? true : false;
+    # offset is important due to boundary proximity
+    haslower, hasupper = haslowerupper(bs, x)
+
+    if (II[j] <= D.boundary_point_count) & !haslower
+        @error "haven't implemented this yet"
+        # weights = D.low_boundary_coefs[II[j]]
+        # offset = 1 - II[j]
+        # Itap = [II + (i + offset) * I1 for i in 0:(D.boundary_stencil_length-1)]
+    elseif (II[j] > (length(s, x) - D.boundary_point_count)) & !hasupper
+        @error "haven't implemented this yet"
+        # weights = D.high_boundary_coefs[length(s, x)-II[j]+1]
+        # offset = length(s, x) - II[j]
+        # Itap = [II + (i + offset) * I1 for i in (-D.boundary_stencil_length+1):1:0]
+    else
+        weights = [1.0; -1.0];
+        #weights = D.stencil_coefs
+        Itap = [bwrap(II + i * I1, bs, s, jx) for i in staggered_range(length(weights), is_staggered)]
+    end
+    # Tap points of the stencil, this uses boundary_point_count as this is equal to half the stencil size, which is what we want.
+    return sym_dot(weights, ufunc(u, Itap, x))
 end
