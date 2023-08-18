795efc7733780a71226cb508252e4b865c99ab25
chyett home <cmhyett@gmail.com>
2023-08-17 20:47:29 -0700

    generalizing symbolic tracing operation

--- src/discretization/staggered_discretize.jl
+++ src/discretization/staggered_discretize.jl
@@ -12,7 +12,7 @@ function SciMLBase.discretize(pdesys::PDESystem,
             add_metadata!(get_metadata(simpsys), sys)
             prob = ODEProblem(simpsys, Pair[], tspan; discretization.kwargs...,
                               kwargs...)
-            return symbolic_trace(prob, sys)
+            return symbolic_trace(prob, simpsys)
 
         end
     catch e
@@ -21,30 +21,20 @@ function SciMLBase.discretize(pdesys::PDESystem,
 end
 
 function symbolic_trace(prob, sys)
-    # u = get_states(sys);
-    # u1inds = findall(x->?, u)
-    # u2inds = findall(x->?, u)
-    # u1 = u[u1inds]
-    # u2 = u[u2inds]
-    # du1 = prob.f([collect(u1); collect(u2)], nothing, 0.0)[u1inds];
-    # du2 = prob.f([collect(u1); collect(u2)], nothing, 0.0)[u2inds];
-    # gen_drho = eval(Symbolics.build_function(du1, collect(u1), collect(u2))[2]);
-    # gen_dphi = eval(Symbolics.build_function(du2, collect(u1), collect(u2))[2]);
-    # dynamical_f1(_du1,u,p,t) = gen_drho(_du1, u[u1inds], u[u2inds]);
-    # dynamical_f2(_du2,u,p,t) = gen_dphi(_du2, u[u1inds], u[u2inds]);
-    # u0 = [prob.u0[u1inds]; prob.u0[u2inds]];
-    # return DynamicalODEProblem{false}(dynamical_f1, dynamical_f2, u0[1:length(u1)], u0[length(u1)+1:end], prob.tspan)
-    len = floor(Int, length(prob.u0)/2);
-
-    @variables rho[1:len] phi[1:len+1]
-    drho = (prob.f([collect(rho); collect(phi)], nothing, 0.0)[1:len]);
-    dphi = (prob.f([collect(rho); collect(phi)], nothing, 0.0)[len+1:end]);
-
-    gen_drho = eval(Symbolics.build_function(drho, collect(rho), collect(phi))[2]);
-    gen_dphi = eval(Symbolics.build_function(dphi, collect(rho), collect(phi))[2]);
-
-    dynamical_f1(_drho,u,p,t) = gen_drho(_drho, u[1:len], u[len+1:end]);
-    dynamical_f2(_dphi,u,p,t) = gen_dphi(_dphi, u[1:len], u[len+1:end]);
-    u0 = [prob.u0[1:len]; prob.u0[len+1:end]];
-    return DynamicalODEProblem{false}(dynamical_f1, dynamical_f2, u0[1:len], u0[len+1:end], prob.tspan)
+    get_var_from_state(state) = operation(arguments(state)[1]);
+    states = get_states(sys);
+    u1_var = get_var_from_state(states[1]);
+    u2_var = get_var_from_state(states[findfirst(x->get_var_from_state(x)!=u1_var, states)]);
+    u1inds = findall(x->get_var_from_state(x)===u1_var, states);
+    u2inds = findall(x->get_var_from_state(x)===u2_var, states);
+    u1 = states[u1inds]
+    u2 = states[u2inds]
+    du1 = prob.f([collect(u1); collect(u2)], nothing, 0.0)[u1inds];
+    du2 = prob.f([collect(u1); collect(u2)], nothing, 0.0)[u2inds];
+    gen_du1 = eval(Symbolics.build_function(du1, collect(u1), collect(u2))[2]);
+    gen_du2 = eval(Symbolics.build_function(du2, collect(u1), collect(u2))[2]);
+    dynamical_f1(_du1,u,p,t) = gen_du1(_du1, u[u1inds], u[u2inds]);
+    dynamical_f2(_du2,u,p,t) = gen_du2(_du2, u[u1inds], u[u2inds]);
+    u0 = [prob.u0[u1inds]; prob.u0[u2inds]];
+    return DynamicalODEProblem{false}(dynamical_f1, dynamical_f2, u0[1:length(u1)], u0[length(u1)+1:end], prob.tspan)
 end
