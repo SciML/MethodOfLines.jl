7ea157efb5cc9e179549ad594eab8721b916ad5c
chyett home <cmhyett@gmail.com>
2023-08-31 20:13:43 -0700

    updating staggered discretize to output split ode system compatible with OTS solvers

--- src/discretization/staggered_discretize.jl
+++ src/discretization/staggered_discretize.jl
@@ -29,12 +29,15 @@ function symbolic_trace(prob, sys)
     u2inds = findall(x->get_var_from_state(x)===u2_var, states);
     u1 = states[u1inds]
     u2 = states[u2inds]
-    du1 = prob.f([collect(u1); collect(u2)], nothing, 0.0)[u1inds];
-    du2 = prob.f([collect(u1); collect(u2)], nothing, 0.0)[u2inds];
-    gen_du1 = eval(Symbolics.build_function(du1, collect(u1), collect(u2))[2]);
-    gen_du2 = eval(Symbolics.build_function(du2, collect(u1), collect(u2))[2]);
-    dynamical_f1(_du1,u,p,t) = gen_du1(_du1, u[u1inds], u[u2inds]);
-    dynamical_f2(_du2,u,p,t) = gen_du2(_du2, u[u1inds], u[u2inds]);
-    u0 = [prob.u0[u1inds]; prob.u0[u2inds]];
-    return DynamicalODEProblem{false}(dynamical_f1, dynamical_f2, u0[1:length(u1)], u0[length(u1)+1:end], prob.tspan)
+    tracevec_1 = [i in u2inds ? states[i] : Num(0.0) for i in 1:length(states)] # maybe just 0.0
+    tracevec_2 = [i in u1inds ? states[i] : Num(0.0) for i in 1:length(states)]
+    du1 = prob.f(tracevec_1, nothing, 0.0);
+    du2 = prob.f(tracevec_2, nothing, 0.0);
+    gen_du1 = eval(Symbolics.build_function(du1, states)[2]);
+    gen_du2 = eval(Symbolics.build_function(du2, states)[2]);
+    dynamical_f1(_du1,u,p,t) = gen_du1(_du1, u);
+    dynamical_f2(_du2,u,p,t) = gen_du2(_du2, u);
+    u0 = prob.u0;#[prob.u0[u1inds]; prob.u0[u2inds]];
+    return SplitODEProblem(dynamical_f1, dynamical_f2, u0, prob.tspan);
+#return DynamicalODEProblem{false}(dynamical_f1, dynamical_f2, u0[1:length(u1)], u0[length(u1)+1:end], prob.tspan)
 end
