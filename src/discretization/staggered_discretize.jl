90ad3ea4d35fff341ce03e4641b8e1d0e89da93c
chyett home <cmhyett@gmail.com>
2024-01-13 14:33:56 -0500

    fixing tracing logic and adding inhomogenous, nonlinear wave eq test

--- src/discretization/staggered_discretize.jl
+++ src/discretization/staggered_discretize.jl
@@ -27,17 +27,13 @@ function symbolic_trace(prob, sys)
     u2_var = get_var_from_state(states[findfirst(x->get_var_from_state(x)!=u1_var, states)]);
     u1inds = findall(x->get_var_from_state(x)===u1_var, states);
     u2inds = findall(x->get_var_from_state(x)===u2_var, states);
-    u1 = states[u1inds]
-    u2 = states[u2inds]
-    tracevec_1 = [i in u2inds ? states[i] : Num(0.0) for i in 1:length(states)] # maybe just 0.0
-    tracevec_2 = [i in u1inds ? states[i] : Num(0.0) for i in 1:length(states)]
-    du1 = prob.f(tracevec_1, nothing, 0.0);
-    du2 = prob.f(tracevec_2, nothing, 0.0);
+    tmp = prob.f(states, nothing, 0.0)
+    du1 = [i in u1inds ? tmp[i] : Num(0.0) for i in 1:length(states)];
+    du2 = [i in u2inds ? tmp[i] : Num(0.0) for i in 1:length(states)];
     gen_du1 = eval(Symbolics.build_function(du1, states)[2]);
     gen_du2 = eval(Symbolics.build_function(du2, states)[2]);
     dynamical_f1(_du1,u,p,t) = gen_du1(_du1, u);
     dynamical_f2(_du2,u,p,t) = gen_du2(_du2, u);
     u0 = prob.u0;#[prob.u0[u1inds]; prob.u0[u2inds]];
     return SplitODEProblem(dynamical_f1, dynamical_f2, u0, prob.tspan);
-#return DynamicalODEProblem{false}(dynamical_f1, dynamical_f2, u0[1:length(u1)], u0[length(u1)+1:end], prob.tspan)
 end
