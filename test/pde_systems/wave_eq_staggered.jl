62f8a8d7f403e945a7d2ae0ae7c04f7d834cec37
chyett home <cmhyett@gmail.com>
2024-01-23 13:31:07 -0500

    Revert "Merge branch 'master' of https://github.com/cmhyett/MethodOfLines.jl"
    This reverts commit 0cf3c7940fe5f8f11303fa4a63d403d9f8740cfc, reversing
    changes made to 494dd7ec098e554a9e0896ad99864100b1f39966.

--- test/pde_systems/wave_eq_staggered.jl
+++ test/pde_systems/wave_eq_staggered.jl
@@ -19,7 +19,7 @@ using OrdinaryDiffEq
     bcs = [ρ(0,x) ~ initialFunction(x),
            ϕ(0.0,x) ~ 0.0,
            Dx(ρ(t,L)) ~ 0.0,
-           ϕ(t,-L) ~ 0.0];
+           ϕ(t,-L) ~ 0.0];#-a^2*Dx(ρ(t,L))];
 
     domains = [t in Interval(0.0, tmax),
                x in Interval(-L, L)];
@@ -105,42 +105,4 @@ end
     test_ind = round(Int, ((2*L-dx)/a)/(dt))
     @test maximum(sol[1:128,1] .- sol[1:128,test_ind]) < max(dx^2, dt)
     @test maximum(sol[1:128,1] .- sol[1:128,2*test_ind]) < 10*max(dx^2, dt)
-end
-
-@testset "1D inhomogenous nonlinear wave equation, staggered grid, Mixed BC" begin
-    @parameters x
-    @variables t, ρ(..), ϕ(..)
-    Dt = Differential(t);
-    Dx = Differential(x);
-
-    β = 0.11/(2*0.01);
-    a = 0.5;
-    L = 1.0;
-    dx = 1/2^7;
-    tspan = (0.0, 6.0)
-
-    function RHS(ρ, ϕ)
-        return -β*(ϕ^2)*sign(ϕ)/ρ
-    end
-
-    eqs = [Dt(ρ(t,x)) + Dx(ϕ(t,x)) ~ 0,
-           Dt(ϕ(t,x)) + a^2*Dx(ρ(t,x)) ~ RHS(ρ(t, x), ϕ(t,x))]
-    bcs = [ρ(0,x) ~ 50.0*exp(-((x-0.5)*20)^2) + 50.0,
-           ϕ(0,x) ~ 0.0,
-           Dx(ρ(t,L)) ~ 0.0,
-           ϕ(t,0.0) ~ 0.0]
-    domains = [t in Interval(tspan[1], tspan[end]),
-               x in Interval(0.0, L)]
-
-    @named pdesys = PDESystem(eqs, bcs, domains, [t,x], [ρ(t,x), ϕ(t,x)])
-    discretization = MOLFiniteDifference([x=>dx], t, grid_align=MethodOfLines.StaggeredGrid(), edge_aligned_var=ϕ(t,x));
-    prob = discretize(pdesys, discretization);
-    @time sol = solve(prob, SplitEuler(), dt=(dx/a)^2, adaptive=false)
-    @test sol.retcode == ReturnCode.Success
-    # p = plot()
-    # for i in 1:tspan[end]*((a/dx)^2)/10:length(sol)
-    #     ind = floor(Int, i)
-    #     plot!(p, sol.u[ind], label="t = $(@sprintf("%.2f", sol.t[ind]))")
-    # end
-    # p
 end
