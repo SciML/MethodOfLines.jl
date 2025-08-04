46c8082d2647117f01c44267cdabdd97362398e0
fabern <10245680+fabern@users.noreply.github.com>
2025-08-04 09:45:00 +0200

    Add transformation of [x...END]-integrals

--- src/discretization/schemes/integral_expansion/integral_expansion.jl
+++ src/discretization/schemes/integral_expansion/integral_expansion.jl
@@ -22,11 +22,42 @@ function _euler_integral(II, s, jx, u, ufunc, dx) #where {T,N,Wind,DX<:Number}
     return sym_dot(weights, ufunc(u, Itap, x)) +
            _euler_integral(II - I1, s, jx, u, ufunc, dx)
 end
+function _euler_integral_II_to_upper(II, s, jx, u, ufunc, dx) #where {T,N,Wind,DX<:Number}
+    j, x = jx
+    if II[j] == length(s, x) # recursively arrived at upper end of the domain
+        return Num(0)
+    end
+    # unit index in direction of the derivative
+    I1 = unitindex(ndims(u, s), j)
+    # dx for multiplication
+    Itap = [II, II + I1]
 
-function euler_integral(II, s, jx, u, ufunc)
+    if dx isa Number # Uniform dx
+        weights = [dx / 2, dx / 2]
+    elseif dx isa AbstractVector # Nonuniform dx
+        weights = fill(dx[II[j] + 1] / 2, 2)
+    else
+        error("Unsupported type of dx: $(type(dx))")
+    end
+    
+    # sym_do computes from II to II + I1, 
+    # and recursive call computes from II + I1 to upper end of domain
+    return sym_dot(weights, ufunc(u, Itap, x)) +
+           _euler_integral_II_to_upper(II + I1, s, jx, u, ufunc, dx)
+end
+
+# An integral from II to end of domain
+function euler_integral(method::Symbol, II, s, jx, u, ufunc)
     j, x = jx
     dx = s.dxs[x]
-    return _euler_integral(II, s, jx, u, ufunc, dx)
+    if method == :lower_boundary_to_x
+        return _euler_integral(II, s, jx, u, ufunc, dx)
+    elseif method == :x_to_upper_boundary
+        return _euler_integral_II_to_upper(II, s, jx, u, ufunc, dx)
+        # error("Method :x_to_upper_boundary is not implemented for euler_integral.")
+    else
+        error("Unsupported method for euler_integral: $method")
+    end
 end
 
 # An integral across the whole domain (xmin .. xmax)
@@ -47,13 +78,18 @@ end
         II::CartesianIndex, s::DiscreteSpace, depvars, indexmap, terms)
     ufunc(u, I, x) = s.discvars[u][I]
 
-    eulerrules = reduce(safe_vcat,
-        [[Integral(x in DomainSets.ClosedInterval(s.vars.intervals[x][1],
-              Num(x)))(u) => euler_integral(
-              Idx(II, s, u, indexmap), s, (x2i(s, u, x), x), u, ufunc)
-          for x in ivs(u, s)]
-         for u in depvars],
-        init = [])
+    eulerrules = reduce(safe_vcat, [
+        reduce(safe_vcat, [
+        [
+            # integrals from lower domain end to x:
+            Integral(x in DomainSets.ClosedInterval(s.vars.intervals[x][1], Num(x)))(u) =>  euler_integral(:lower_boundary_to_x, Idx(II, s, u, indexmap), s, (x2i(s, u, x), x), u, ufunc), 
+            # integrals from x to upper domain end:
+            Integral(x in DomainSets.ClosedInterval(Num(x), s.vars.intervals[x][2]))(u) =>  euler_integral(:x_to_upper_boundary, Idx(II, s, u, indexmap), s, (x2i(s, u, x), x), u, ufunc),
+            # TODO: # any other arbitrary integrals???
+            # TODO...
+        ]
+    for x in ivs(u, s)]) for u in depvars] )
+
     return eulerrules
 end
 
