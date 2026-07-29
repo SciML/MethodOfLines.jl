c905b281ecaaa29d8246a737319b0c3a1e396df3
utkuyilmaz1903 <utkyilmz@gmail.com>
2026-07-29 09:14:24 +0300

    chore: remove duplicated weno test entry

--- test/runtests.jl
+++ test/runtests.jl
@@ -82,9 +82,6 @@ run_tests(;
             @safetestset "WENO Pipeline Spatial Convergence (MMS)" begin
                 include(joinpath(@__DIR__, "Convection_WENO", "MOL_1D_WENO_NU_Convergence.jl"))
             end
-            @safetestset "WENO Linear Convection" begin
-                include(joinpath(@__DIR__, "Convection_WENO", "MOL_1D_Linear_Convection_WENO.jl"))
-            end
             return @safetestset "WENO Non-Uniform Interface/Periodic" begin
                 include(joinpath(@__DIR__, "Convection_WENO", "MOL_1D_WENO_NU_Interface.jl"))
             end
